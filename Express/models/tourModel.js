78c0d6f14b6be4d305a9ac4146881cb20f8e5dfb
Umer Khan <mohammadumerk8@gmail.com>
2026-08-08 22:41:43 +0500

    query midleware

--- Express/models/tourModel.js
+++ Express/models/tourModel.js
@@ -113,6 +113,11 @@ tourSchema.pre('save', function (next) {
     next();
 });
 
+tourSchema.pre(/^find/, function(next){
+    this.find({secretTour : {$ne : true}});
+  next();
+});
+
 const Tour = mongoose.model('Tour' , tourSchema);
 
 
