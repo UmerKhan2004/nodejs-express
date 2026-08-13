1923999eaae8f3a178198865becd0ba5b76ce9a1
Umer Khan <mohammadumerk8@gmail.com>
2026-08-13 23:42:47 +0500

    fix(app): remove syntax error and clean up debug code

--- Express/app.js
+++ Express/app.js
@@ -4,24 +4,13 @@ const morgan = require('morgan');
 const app = express();
 
 // MIDDLEWARES
-console.log(process.env.NODE_ENV);
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 app.use(express.static(`${__dirname}/public`));
-
-// Custom middleware
-app.use((req, res, next) => {
-    console.log("hello from the middleware");
-    next();
-});
-
-
-
-
 
 // ROUTES
 const userRouter = require('./routes/userRoutes.js');
@@ -31,10 +20,10 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*splat', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server`
-    });
-});l
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
 
 module.exports = app;
