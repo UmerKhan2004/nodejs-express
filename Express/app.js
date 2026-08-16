8da674ec3b69487650bfb34020acaef01227aef6
Umer Khan <mohammadumerk8@gmail.com>
2026-08-16 16:30:49 +0500

    Global error handler

--- Express/app.js
+++ Express/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const AppError = require('./utils/AppError.js');
+const gloabalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 // MIDDLEWARES
@@ -20,20 +21,15 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*splat', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`
-  });
-});
+  
+// });res.status(404).json({
+//     status: 'fail',
+//     message: `Can't find ${req.originalUrl} on this server`
+//   });
 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+next(new AppError(`Can't find ${req.originalUrl} on this serverr`, 404));
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
 });
+app.use(gloabalErrorHandler);
 
 module.exports = app;
