744ff105a55b13196bb5b7e0ef631bd90141107b
Umer Khan <mohammadumerk8@gmail.com>
2026-08-21 21:24:43 +0500

    Uncaught exception and Unhandle rejection

--- Express/server.js
+++ Express/server.js
@@ -1,3 +1,12 @@
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  process.exit(1);
+});
+
+
+
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv'); 
 const app = require('./app');
@@ -22,8 +31,15 @@ mongoose.connect(DB, {
 
 
 const port = process.env.PORT || 3000; 
- 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
 
+
+process.on('unhandledRejection' , err => {
+    console.log(err.name , err.message);
+    console.log("Shutting down server");
+    server.close(() => {
+        process.exit(1);
+    });
+});
