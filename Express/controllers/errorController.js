4074be7ea1a142b491970e8b73e1d248fb0e3bc8
Umer Khan <mohammadumerk8@gmail.com>
2026-08-25 23:23:32 +0500

    Handle expired token

--- Express/controllers/errorController.js
+++ Express/controllers/errorController.js
@@ -17,9 +17,12 @@ const handleValidationErrorDB = err => {
 };
 
 const handleJWTError = err => {
-    const message = "Loged in first"
-    return new AppError(message, 400);
+    const message = "Invalid token ,please log in first"
+    return new AppError(message, 401);
 }
+
+const handleJWTExpiredError = () =>
+    new AppError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
@@ -58,6 +61,7 @@ module.exports = (err, req, res, next) => {
         if(error.code === 11000) error = handleDuplicateFieldsDB(error)
         if(error.name === 'ValidationError') error = handleValidationErrorDB(error);
         if(error.name === 'JsonWebTokenError') error = handleJWTError(error);
+        if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
         sendErrorProd(error, res);
     }
